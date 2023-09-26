be1ac38e37584a1273970c1f8777faf98b474bc3
Arunendra Singh <singharunendra978@gmail.com>
2023-09-27 01:11:22 +0530

    Added new model todo in Models to perform CRUD operation on Database.

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+# TODO: Create a model to save the todo items
+
+"""
+title,
+description: Text,
+priority,
+complete_by: Date and Time to Complete,
+send_reminder: Bool,
+reminder_time: Time before to send the notification.
+added_at,
+updated_at,
+"""
+
+class Todo(models.Model):
+    title =  models.CharField(max_length=90)
+    description = models.TextField(null=True, blank=True)
+    priority = models.CharField(max_length=50) #  Need to use DropDown and also need to create Priority table in database to priorities the task.
+    completion_time = models.DateTimeField()
+    reminder = models.BooleanField(default=False)
+    reminder_before_time = models.DateTimeField()
+    updated_at = models.DateTimeField(auto_now=True)
+    created_At = models.DateTimeField(auto_now_add=True)
