6c4ff035828517a8d0c5e853d48453e57fc45e59
Arunendra Singh <singharunendra978@gmail.com>
2023-10-16 22:15:15 +0530

    add a validator to validate the correct time and raise a validation error if todo time is less than or equalto to correct time.

--- todos/models.py
+++ todos/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 
-
 class Todo(models.Model):
-    title =  models.CharField(max_length=90)
+    title = models.CharField(max_length=90)
     description = models.TextField(null=True, blank=True)
-    priority = models.CharField(max_length=50) #  Need to use DropDown and also need to create Priority table in database to priorities the task.
+    # Need to use DropDown and also need to create Priority table in database to priorities the task.
+    priority = models.CharField(max_length=50)
     completion_time = models.DateTimeField()
     reminder = models.BooleanField(default=False)
-    reminder_before_time = models.DateTimeField()
+    reminder_before_time = models.IntegerField()
     updated_at = models.DateTimeField(auto_now=True)
     created_At = models.DateTimeField(auto_now_add=True)
--- todos/serializers.py
+++ todos/serializers.py
@@ -2,13 +2,37 @@ from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from todos.models import Todo
 
+from datetime import datetime, timezone
+
 # Your serializer
+
+def convert_django_time_in_datetime(str_time: str):
+    return datetime.strptime(str_time,  "%Y-%m-%dT%H:%M:%S.%fZ")
 
 class TodoSerializer(serializers.ModelSerializer):
 
     # We can also create field level validator in ModelSerializer
 
     title = serializers.CharField(validators=[UniqueValidator(queryset=Todo.objects.all(), message="Title should be Unique.")])
+
+    def validate(self, attrs):
+        try:
+            current_time = datetime.now(timezone.utc)
+
+            time_diff  =  attrs["completion_time"] - current_time
+
+            # if diff.days == 0 and diff.seconds == 0 : raise validation error to select greater time than current time.
+
+            # if diff.days >= 0 and diff.
+            if time_diff.day == 0 and time_diff.seconds == 0:
+                return serializers.ValidationError("Todo time must be greater than current time.")
+            
+            if time_diff.day >= 0 and time_diff.seconds >= 0:
+                return super().validate(attrs)
+            
+            return serializers.ValidationError("Todo time must be greater than current time.")
+        except ValueError:
+            return serializers.ValidationError("Please provide correct date-time format. accepted format is: '%Y-%m-%dT%H:%M:%S.%fZ'")
 
     def create(self, validated_data):
         return Todo.objects.create(**validated_data)
@@ -27,8 +51,4 @@ class TodoSerializer(serializers.ModelSerializer):
         model = Todo
         fields = '__all__'
 
-
-# class UpdateTodoSerializer(TodoSerializer):
-
-#    class Meta:
         
