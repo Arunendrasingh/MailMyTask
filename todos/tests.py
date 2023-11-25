a9b02e98e5e4dfcc7849c5ad232e6c8aa8378f6c
Arunendra Singh <singharunendra978@gmail.com>
2023-11-25 15:19:49 +0530

    Add Test Cases for Task Priority.

--- todos/tests.py
+++ todos/tests.py
@@ -10,26 +10,105 @@ class TaskPriorityViewsTest(APITestCase):
     @classmethod
     def setUpClass(cls) -> None:
         # Adding Two user with name testUser1 and testUser2 before running test in this class.
-        test_user1 = User.objects.create(username="testUser1", password="admin@1234")
+        test_user1 = User.objects.create(
+            username="testUser1", password="admin@1234")
         test_user1.save()
-        test_user2 = User.objects.create(username="testUser2", password="admin@1234")
+        test_user2 = User.objects.create(
+            username="testUser2", password="admin@1234")
         test_user2.save()
         return super().setUpClass()
 
-    def test_create_new_priority(self):
+    def test_empty_task_priority(self):
         response = self.client.get("/task_priority/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(type(response.json()["resultObject"]), list)
         self.assertEqual(response.json()["resultObject"], [])
 
-    def create_two_new_task_priority_testUser1(APITestCase):
-        # TODO: Create new TaskPriority with first user and this should be return as 201
-        # TODO: Now Create Priority with same name with same user user na this time request must fail.
-        ...
+    def test_create_new_task_priority_testUser1(self):
+        """Create a task priority with testUser1"""
 
-    # TODO: Create a new test function to test teh same with other user and with same priority.
+        request_data = {
+            "title": "Easy-1",
+            "weight": 5,
+            "color": "Orange",
+            "user": 1
+        }
+        # Use client to test the Test case. first create a taskpriority with testUser1 and again try to create a task priority with same name for same user and this time code must have to return False.
+        response = self.client.post("/task_priority/", data=request_data)
+        self.assertEqual(response.status_code, 201)
+        # checking response
+        self.assertEqual(response.data, {
+            "hasError": False,
+            "errors": "",
+            "resultObject": {
+                "id": 1,
+                "title": "Easy-1",
+                "color": "Orange",
+                "weight": 5,
+                "user": 1
+            }
+        })
 
-# TODO: Proceed with APIClient.
-# TODO: 1-Add test case to add same priority with same user and this test should be failed.
-# TODO: 2-Add test case to add same priority with other user and this test have to pass.
-# TODO: repeat the test-1 with this user and it should be failed.
+    def test_title_user_should_unique(self):
+        """Create a Task priority and after that again create a task priority with same data, and this time API must have to return bad request."""
+        request_data = {
+            "title": "Test Priority One",
+            "weight": 5,
+            "color": "Orange",
+            "user": 1
+        }
+        # Use client to test the Test case. first create a taskpriority with testUser1 and again try to create a task priority with same name for same user and this time code must have to return False.
+        response = self.client.post("/task_priority/", data=request_data)
+        self.assertEqual(response.status_code, 201)
+
+        request_date_1 = {
+            "title": "Test Priority One",
+            "weight": 4,
+            "color": "Orange",
+            "user": 1
+        }
+        response_1 = self.client.post("/task_priority/", data=request_date_1)
+        self.assertNotEqual(response_1.status_code, 201)
+
+    def test_title_dif_user_work(self):
+        request_data = {
+            "title": "Test Priority One",
+            "weight": 5,
+            "color": "Orange",
+            "user": 1
+        }
+        # Use client to test the Test case. first create a taskpriority with testUser1 and again try to create a task priority with same name for same user and this time code must have to return False.
+        response = self.client.post("/task_priority/", data=request_data)
+        self.assertEqual(response.status_code, 201)
+
+        request_date_1 = {
+            "title": "Test Priority One",
+            "weight": 4,
+            "color": "Orange",
+            "user": 2
+        }
+        response_1 = self.client.post("/task_priority/", data=request_date_1)
+        self.assertEqual(response_1.status_code, 201)
+    
+    def test_weight_will_have_to_unique_with_user(self):
+        request_data = {
+            "title": "Test Priority One",
+            "weight": 5,
+            "color": "Orange",
+            "user": 1
+        }
+        # Use client to test the Test case. first create a taskpriority with testUser1 and again try to create a task priority with same name for same user and this time code must have to return False.
+        response = self.client.post("/task_priority/", data=request_data)
+        self.assertEqual(response.status_code, 201)
+
+        request_date_1 = {
+            "title": "Test Priority 2",
+            "weight": 5,
+            "color": "Orange",
+            "user": 1
+        }
+        response_1 = self.client.post("/task_priority/", data=request_date_1)
+        self.assertNotEqual(response_1.status_code, 201)
+
+
+
