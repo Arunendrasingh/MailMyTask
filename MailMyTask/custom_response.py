ecacc6b397e2732c2b13161591a542e6f323063e
Arunendra Singh <singharunendra978@gmail.com>
2023-12-18 09:28:41 +0530

    Add folder & subdfolder model.

--- MailMyTask/custom_response.py
+++ MailMyTask/custom_response.py
@@ -5,7 +5,6 @@ from rest_framework.renderers import JSONRenderer
 
 
 class CustomResponse(Response):
-
     def __init__(self, data=None, status=None, has_error: Optional[bool | None] = None, headers=None, errors=None, content_type=None):
         data = {"hasError": has_error, "errors": errors, "resultObject": data}
         super().__init__(data, status, headers, exception=has_error, content_type=content_type)
--- todos/models.py
+++ todos/models.py
@@ -2,18 +2,40 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Folder(models.Model):
+    title = models.CharField(max_length=90)
+    description = models.TextField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    updatedAt = models.DateTimeField(auto_now=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class SubFolder(models.Model):
+    title = models.CharField(max_length=90)
+    description = models.TextField(blank=True)
+    folder = models.ForeignKey(Folder, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    updatedAt = models.DateTimeField(auto_now=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
 class Todo(models.Model):
     title = models.CharField(max_length=90)
-    description = models.TextField(null=True, blank=True)
-    priority = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
     completion_time = models.DateTimeField()
     reminder = models.BooleanField(default=False)
     reminder_before_time = models.IntegerField()
-    updated_at = models.DateTimeField(auto_now=True)
-    created_At = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     task_priority = models.ForeignKey(
         "TaskPriority", null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    updatedAt = models.DateTimeField(auto_now=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.title
@@ -23,9 +45,9 @@ class TaskPriority(models.Model):
     title = models.CharField(max_length=90)
     color = models.CharField(max_length=20)
     weight = models.IntegerField()
-
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    updatedAt = models.DateTimeField(auto_now=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.title
-
