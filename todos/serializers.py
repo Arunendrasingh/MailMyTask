7901e4571a1b85a94cd71eb08ca43a8b678b9bdb
Arunendra Singh <singharunendra978@gmail.com>
2023-12-19 09:00:49 +0530

    Add Serializer for Folder & SubFolder Model.

--- todos/serializers.py
+++ todos/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
-from todos.models import TaskPriority, Todo
+from todos.models import Folder, SubFolder, TaskPriority, Todo
 
 from datetime import datetime, timezone
 
@@ -15,7 +14,7 @@ class TaskPrioritySerializer(serializers.ModelSerializer):
     class Meta:
         model = TaskPriority
         exclude = ["user"]
-        
+
     def validate_title(self, value):
         request = self.context["request"]
 
@@ -42,7 +41,7 @@ class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ["id", "title", "description", "reminder", "reminder_before_time",
-                  "completion_time",  "task_priority", "priority", "owner", "updated_at", "created_At"]
+                  "completion_time",  "task_priority", "owner", "updatedAt", "createdAt"]
 
     def validate_task_priority(self, value):
         # get the existence of value in Priority table
@@ -83,3 +82,59 @@ class TodoSerializer(serializers.ModelSerializer):
         except ValueError:
             raise serializers.ValidationError(
                 "Please provide correct date-time format. accepted format is: '%Y-%m-%dT%H:%M:%S.%fZ'")
+
+
+class FolderSerializer(serializers.ModelSerializer):
+    owner = serializers.CharField(source="user", read_only=True)
+
+    class Meta:
+        model = Folder
+        fields = ["id", "title", "description",
+                  "owner", "updatedAt", "createdAt"]
+
+    def validate_title(self, value):
+        request = self.context["request"]
+        if Folder.objects.filter(user=request.user, title=value).exists():
+            raise serializers.ValidationError(
+                f"Folder with name '{value}' already present.")
+
+        return value
+
+
+class SubFolderSerializer(serializers.ModelSerializer):
+    owner = serializers.CharField(source="user", read_only=True)
+    folder = FolderSerializer(read_only=True)
+    folder_id = serializers.CharField(source="folder", write_only=True)
+
+    class Meta:
+        model = SubFolder
+        fields = ["id", "title", "description", "folder", "folder_id",
+                  "owner", "updatedAt", "createdAt"]
+
+    # TODO: Add a validator for folder_id while creating, and if folder_id is not provided then do not create subfolder and raise Validation Error
+
+    def validate_title(self, value):
+        request = self.context["request"]
+        if SubFolder.objects.filter(user=request.user, title=value).exists():
+            raise serializers.ValidationError(
+                f"Folder with name '{value}' already present.")
+
+        return value
+
+    def validate_folder_id(self, value):
+        # get the existence of value in Priority table
+        print("Validating folder while adding the subfolder")
+
+        if not value:
+            raise serializers.ValidationError(
+                "ID of folder is required to Create/Update the Sub Folder")
+
+        if isinstance(value, Folder):
+            value = value.id
+
+        priority = Folder.objects.filter(id=value).first()
+        if priority:
+            return priority
+
+        raise serializers.ValidationError(
+            "Unable to find folder with key: ", value)
