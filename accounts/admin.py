9551378a05ae4880154f28cec20d5f1a9dd132db
Arunendra Singh <singharunendra978@gmail.com>
2023-12-22 23:54:30 +0530

    Add Custome User in accounts.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from accounts.models import CustomUser
 
 # Register your models here.
+
+
+class UserAdmin(BaseUserAdmin):
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ["id", "email", "contact", "is_superuser"]
+    list_filter = ["is_superuser", "is_active"]
+    fieldsets = [
+        (None, {"fields": ["email", "password"]}),
+        ("Personal info", {"fields": [
+         "first_name", "last_name", "contact", "date_of_birth"]}),
+        ("Permissions", {"fields": ["is_superuser", "is_staff_user", "is_active"]}),
+    ]
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "contact", "password1", "password2"],
+            },
+        ),
+    ]
+    search_fields = ["email", "first_name", "last_name", "contact"]
+    ordering = ["id", "email"]
+    filter_horizontal = []
+
+
+# Now register the new UserAdmin...
+admin.site.register(CustomUser, UserAdmin)
+
+# admin.site.unregister(Group)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,101 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 # Create your models here.
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, contact, password=None, password1=None):
+        """
+        Creates and saves a User with the given email, contact and password.
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        if not password:
+            raise ValueError("Both Password must be same.")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            contact=contact,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, contact, password=None, password1=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        if not password:
+            raise ValueError("Both Password must be same.")
+
+        user = self.create_user(
+            email,
+            password=password,
+            contact=contact
+        )
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name="Email Address",
+        max_length=255,
+        unique=True,
+    )
+    date_of_birth = models.DateField(null=True, blank=True)
+    contact = models.CharField(max_length=10)
+    first_name = models.CharField(
+        verbose_name="first name", max_length=150, blank=True)
+    last_name = models.CharField(
+        verbose_name="last name", max_length=150, blank=True)
+    is_staff_user = models.BooleanField(
+        verbose_name="staff status",
+        default=False,
+        help_text="Designates whether the user can log into this admin site.",
+    )
+    is_active = models.BooleanField(
+        verbose_name="active",
+        default=False,
+        help_text="Designates whether this user should be treated as active. "
+        "Unselect this instead of deleting accounts.",
+    )
+    is_superuser = models.BooleanField(
+        verbose_name="Super User",
+        default=False,
+        help_text="Designates whether this user should be treated as SuperUser. "
+        "Unselect this instead of deleting accounts.",
+    )
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["contact"]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.email}"
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_superuser
