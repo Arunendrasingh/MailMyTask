f06e4ad8ace0cb20d011cd1097c3d0fae2484f31
Arunendra Singh <singharunendra978@gmail.com>
2023-12-24 08:48:39 +0530

    .....

--- MailMyTask/urls.py
+++ MailMyTask/urls.py
@@ -16,15 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken import views
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
-    path("auth/", views.obtain_auth_token),
-    path('admin/', admin.site.urls),
-    path('', include("todos.urls")),
-    path('api/docs/schema', SpectacularAPIView.as_view(), name='schema'),
+    path('admin/', admin.site.urls, name="admin"),
+    path('auth/', include("accounts.urls")),
     path('', SpectacularSwaggerView.as_view(
         url_name='schema'), name='swagger-ui'),
-    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc')
+    path('', include("todos.urls")),
+    path('api/docs/schema', SpectacularAPIView.as_view(), name='schema'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+import logging
+from rest_framework import status
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+from drf_spectacular.utils import extend_schema
+
+from MailMyTask.custom_response import CustomResponse
+
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
 
 # Create your views here.
+
+
+# Write here the Login, Logout & RegisterView.
+logger = logging.getLogger("django")
+
+# Login View
+
+
+class LoginView(APIView):
+    def post(self, request, format=None):
+        # Implement post logic
+        data = request.data
+        email = data.get('email')
+        password = data.get("password")
+        user = authenticate(email=email, password=password)
+
+        if user is not None:
+            if user.is_active:
+                data = get_tokens_for_user(user)
+                return CustomResponse(has_error=False, data=data)
+            else:
+                return CustomResponse(has_error="This Account is not active!!", status=status.HTTP_404_NOT_FOUND)
+
+        else:
+            return CustomResponse(has_error=True, errors="Invalid username & password.", status=status.HTTP_404_NOT_FOUND)
